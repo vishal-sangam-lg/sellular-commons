1b480226b33174f816346d10acd57c7b0a39cd4a
Vishal Sangam L G <vishalsangamlg@Vishals-MacBook-Pro.local>
2025-04-17 21:44:53 +0530

    add x-transaction-id to producer

--- src/main/java/com/sellular/commons/kafka/client/KafkaProducerClient.java
+++ src/main/java/com/sellular/commons/kafka/client/KafkaProducerClient.java
@@ -1,12 +1,16 @@
 package com.sellular.commons.kafka.client;
 
+import com.sellular.commons.kafka.constants.HeaderConstants;
 import com.sellular.commons.kafka.constants.KafkaEventInterface;
 import lombok.extern.slf4j.Slf4j;
 import lombok.RequiredArgsConstructor;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.slf4j.MDC;
 
 import java.util.Objects;
+import java.util.Optional;
+import java.util.UUID;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -16,6 +20,11 @@ public class KafkaProducerClient {
 
     public void publish(byte[] messageBytes, KafkaEventInterface event, String key) {
         ProducerRecord<String, byte[]> record = new ProducerRecord<>(event.getTopic(), key, messageBytes);
+        Optional.ofNullable(MDC.get(HeaderConstants.X_TRANSACTION_ID))
+                .ifPresentOrElse(xTransactionId -> record.headers().add(HeaderConstants.X_TRANSACTION_ID, xTransactionId.getBytes()), () -> {
+                    MDC.put(HeaderConstants.X_TRANSACTION_ID, UUID.randomUUID().toString());
+                    record.headers().add(HeaderConstants.X_TRANSACTION_ID, MDC.get(HeaderConstants.X_TRANSACTION_ID).getBytes());
+                });
         try {
             producer.send(record, (metadata, exception) -> {
                 if (Objects.nonNull(exception)) {
---
+++ src/main/java/com/sellular/commons/kafka/constants/HeaderConstants.java
@@ -0,0 +1,7 @@
+package com.sellular.commons.kafka.constants;
+
+public class HeaderConstants {
+
+    public static final String  X_TRANSACTION_ID = "x-transaction-id";
+
+}
