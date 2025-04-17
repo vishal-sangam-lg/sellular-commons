97e06023478206ee5e9b1f92cf113a90a148d4da
Vishal Sangam L G <vishalsangamlg@Vishals-MacBook-Pro.local>
2025-04-17 22:04:42 +0530

    update producer client with config

--- src/main/java/com/sellular/commons/kafka/client/KafkaProducerClient.java
+++ src/main/java/com/sellular/commons/kafka/client/KafkaProducerClient.java
@@ -1,13 +1,16 @@
 package com.sellular.commons.kafka.client;
 
+import com.sellular.commons.kafka.config.KafkaConfiguration;
 import com.sellular.commons.kafka.constants.HeaderConstants;
 import com.sellular.commons.kafka.constants.KafkaEventInterface;
 import lombok.extern.slf4j.Slf4j;
 import lombok.RequiredArgsConstructor;
+import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.slf4j.MDC;
-
+import java.util.Properties;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.UUID;
@@ -15,6 +18,22 @@ import java.util.UUID;
 @Slf4j
 @RequiredArgsConstructor
 public class KafkaProducerClient {
+
+    public KafkaProducerClient(final KafkaConfiguration config) {
+        this.producer = new KafkaProducer<>(buildProducerProps(config));
+        log.info("✅ KafkaProducer initialized for bootstrap servers: {}", config.getBootstrapServers());
+    }
+
+    private Properties buildProducerProps(KafkaConfiguration config) {
+        Properties props = new Properties();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, config.getBootstrapServers());
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, config.getProducerConfiguration().getKeySerializerClass());
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, config.getProducerConfiguration().getValueSerializerClass());
+        props.put(ProducerConfig.CLIENT_ID_CONFIG, config.getClientId());
+        props.put("schema.registry.url", config.getSchemaRegistryServers());
+        return props;
+    }
+
 
     private final Producer<String, byte[]> producer;
 
