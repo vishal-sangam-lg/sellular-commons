505b5004d698a55c9b30cd9456ac62d000112265
Vishal Sangam L G <vishalsangamlg@Vishals-MacBook-Pro.local>
2025-04-18 16:32:56 +0530

    fix swagger global headers - target

--- src/main/java/com/sellular/commons/core/annotations/GlobalSwaggerHeaders.java
+++ src/main/java/com/sellular/commons/core/annotations/GlobalSwaggerHeaders.java
@@ -8,7 +8,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target({ElementType.TYPE})
+@Target({ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 @Parameters({
         @Parameter(name = "X-APP-TOKEN", description = "App Token of the caller", required = true, in = ParameterIn.HEADER),
